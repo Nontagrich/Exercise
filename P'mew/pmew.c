468544d8f07f5e763503547ee8065c6673767826
Nontagrich <113359860+Nontagrich@users.noreply.github.com>
2022-10-08 11:20:43 +0700

    good

--- P'mew/pmew.c
+++ P'mew/pmew.c
@@ -3,6 +3,7 @@ int main()
 {
     //input 
     int n;
+    int rn=1;
     printf("Enter number for loop: ");
     scanf("%d",&n);
 
@@ -10,6 +11,42 @@ int main()
     int rol1,rol2;
     int col1,col2;
 
+    int e1 = 1;
+    for(rol1 = 1 ; rol1 <= n-1 ; rol1++){
+        for(col1 = 1 ; col1 <= 2*n-1 ;col1++){
+            if(col1 <= n - e1){
+                printf("-");
+            }else if(col1 >= n+e1){
+                printf("-");
+            }else {
+                printf("%d",rn);
+                rn += 1;
+            }
+        }
+        e1 += 1;
+        printf("\n");
+    }
     
+    for(int i = 1; i <= (2*n)-1; i++){
+    	printf("%d",rn);
+    	rn += 1;
+	}
+	printf("\n");
+	
+	int e2 = 1;
+	for(rol2 = 1 ; rol2 <= n-1 ; rol2++){
+		for(col2 = 1; col2 <= 2*n-1 ; col2++){
+			if(col2 <= rol2){
+				printf("-");
+			}else if(col2 > 2*n-1-e2){
+				printf("-");
+			}else{
+				printf("%d",rn);
+				rn += 1;
+			}
+		}
+		e2 += 1;
+		printf("\n");
+	}
 
 }
