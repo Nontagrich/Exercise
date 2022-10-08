48ce356f7c4ac5803365df38c58f5ea519a0c4b6
Nontagrich <113359860+Nontagrich@users.noreply.github.com>
2022-10-08 08:38:44 +0700

    Sum

---
+++ Multiples of 3 or 5/1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int sum = 0;
+    for(int i = 1 ; i < 10 ; i++){
+        if(i % 3 == 0 ){
+            sum += i;
+        }else if(i % 5 == 0){
+            sum += i;
+        }
+    }
+    printf("Sum is %d",sum);
+}
