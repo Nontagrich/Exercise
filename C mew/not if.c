0d94856f8d43639f11351a3f867d30654be7d41f
Nontagrich <113359860+Nontagrich@users.noreply.github.com>
2022-10-18 08:11:12 +0700

    Update not if.c

--- C mew/not if.c
+++ C mew/not if.c
@@ -2,5 +2,29 @@
 int main(){
     //taxi
     int dis;
+    printf("Enter distance: ");
+    scanf("%d",&dis);
+
+    while (dis>=0 && dis<20)
+    {
+        printf("Total is %d Baht.",dis*30);
+        dis=-1;
+    }
+    while (dis>=20 && dis<=49)
+    {
+        printf("Total is %d Baht.",dis*25);
+        dis=-1;
+    }
+    while (dis>=50 && dis<=79)
+    {
+        printf("Total is %d Baht.",dis*20);
+        dis=-1;
+    }
+    while (dis>=80)
+    {
+        printf("Total is %d Baht.",dis*30);
+        dis=-1;
+    }
+    return 0;
     
 }
